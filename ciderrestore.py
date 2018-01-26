094d1ff446ae8498d97be66ff8e4f794ffaef018
SteamedFish <i@steamedfish.org>
2018-01-26 17:45:17 +0800

    must use absolute path

--- ciderrestore.py
+++ ciderrestore.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.append("~/dotfiles/cider")
+sys.path.append("/Users/steamedfish/dotfiles/cider")
+print sys.path
 from cider import Cider
+
+sys.exit(0)
 
 cider = Cider(cider_dir="~/dotfiles/.cider")
 cider.restore()
