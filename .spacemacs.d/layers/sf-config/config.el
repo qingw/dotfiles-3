c6bd75c36939583fa0df249463c258a4d6afaf94
SteamedFish <i@steamedfish.org>
2018-07-04 21:26:18 +0800

    switch to with-eval-after-load which is better

--- .spacemacs.d/layers/sf-config/config.el
+++ .spacemacs.d/layers/sf-config/config.el
@@ -1,5 +1,4 @@
-(eval-after-load "fcitx"
-  '(progn
-    (setq fcitx-active-evil-states '(insert emacs hybrid))
-    (fcitx-aggressive-setup)
-    (fcitx-prefix-keys-add "M-m")))
+(with-eval-after-load "fcitx"
+  (setq fcitx-active-evil-states '(insert emacs hybrid))
+  (fcitx-aggressive-setup)
+  (fcitx-prefix-keys-add "M-m"))
