f242304671d09c9f4dac8eac0f7ab3a2a40fdb4d
SteamedFish <i@steamedfish.org>
2019-01-02 09:52:55 +0800

    evil-matchit issue has been fixed
    see https://github.com/redguardtoo/evil-matchit/pull/98

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -2,4 +2,3 @@
 ;;; ~/dotfiles/.doom.d/packages.el
 
 (package! fcitx)
-(package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4"))
