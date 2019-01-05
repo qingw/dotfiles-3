97ae866ee50207f47eaee68033c5939b81d172e8
SteamedFish <i@steamedfish.org>
2019-01-05 15:49:31 +0800

    add more keybindings

--- .doom.d/modules/private/keybindings/config.el
+++ .doom.d/modules/private/keybindings/config.el
@@ -1,6 +1,20 @@
 ;;; private/keybindings/config.el  -*- lexical-binding: t; -*-
 
 (map!
- (:leader
-   (:desc "toggle" :prefix "t"
-     :desc "Transparency" :n "t" #'+default/toggle-transparency)))
+ :leader
+ ;;(:desc "toggle" :prefix "t"
+ (:prefix ("t" . "toggle")
+   :desc "Transparency"                  :n "t"  #'+default/toggle-transparency)
+ (:when (featurep! :intl chinese)
+ (:prefix ("t" . "toggle")
+   :desc "cnfonts-increase-fontsize"     :n "+"  #'cnfonts-increase-fontsize
+   :desc "cnfonts-decrease-fontsize"     :n "-"  #'cnfonts-decrease-fontsize
+   :desc "cnfonts-reset-fontsize"        :n "0"  #'cnfonts-reset-fontsize))
+ (:prefix ("F" . "Frame")
+   :desc "Delete Frame"                  :n "d"  #'delete-frame
+   :desc "Delete Other Frame"            :n "D"  #'delete-other-frame
+   :desc "Other Frame"                   :n "o"  #'other-frame
+   :desc "Make Frame"                    :n "n"  #'make-frame
+   :desc "Make Frame"                    :n "n"  #'make-frame
+   :desc "Switch to buffer Other Frame"  :n "f"  #'switch-to-buffer-other-frame
+   :desc "Display buffer Other Frame"    :n "O"  #'display-buffer-other-frame))
