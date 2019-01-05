6f8c7af4dd91d5bdafb20f4fce3bb53ddca113b2
SteamedFish <i@steamedfish.org>
2019-01-05 15:16:40 +0800

    reformat

--- .doom.d/modules/intl/chinese/config.el
+++ .doom.d/modules/intl/chinese/config.el
@@ -1,13 +1,13 @@
 ;;; intl/chinese/config.el  -*- lexical-binding: t; -*-
 
 (when (display-graphic-p)
- (def-package! cnfonts
-   :commands
-     (cnfonts-enable)))
+  (def-package! cnfonts
+    :commands
+    (cnfonts-enable)))
 
 (def-package! fcitx
   :config
-   (setq-default
-     fcitx-active-evil-states '(insert emacs hybrid))
+  (setq-default
+   fcitx-active-evil-states '(insert emacs hybrid))
   :commands
-   (fcitx-aggressive-setup))
+  (fcitx-aggressive-setup))
