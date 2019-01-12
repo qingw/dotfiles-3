89e3f0bce501f8ee860243e1b000304087471721
SteamedFish <i@steamedfish.org>
2019-01-13 02:54:08 +0800

    add location for sunrise

--- .doom.d/modules/private/steamedfish/config.el
+++ .doom.d/modules/private/steamedfish/config.el
@@ -4,6 +4,9 @@
  user-full-name "SteamedFish"
  user-mail-address "steamedfish@hotmail.com"
  frame-title-format '("%b - " user-full-name "'s Emacs")
+ calendar-latitude 23.1247
+ calendar-longitude 113.3612
+ calendar-location-name "Tianhe, Guangzhou"
  message-log-max 10000
  delete-by-moving-to-trash t)
 (when IS-MAC (setq-default mac-system-move-file-to-trash-use-finder t))
