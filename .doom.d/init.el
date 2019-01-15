925a590638f093334bb30870f608009ba5c291fb
SteamedFish <i@steamedfish.org>
2019-01-15 23:07:43 +0800

    add pyenv

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -122,7 +122,8 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python             ; beautiful is better than ugly
-        +pyenv +ipython)
+        +pyenv
+        +ipython)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
