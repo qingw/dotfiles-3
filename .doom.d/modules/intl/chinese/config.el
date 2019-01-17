5ac269e640814035692e5e29c365b748fb50f926
SteamedFish <i@steamedfish.org>
2019-01-17 10:38:33 +0800

    remove :commands because it's not working

--- .doom.d/modules/intl/chinese/config.el
+++ .doom.d/modules/intl/chinese/config.el
@@ -1,33 +1,31 @@
 ;;; intl/chinese/config.el  -*- lexical-binding: t; -*-
 
-(when (display-graphic-p)
-  (def-package! cnfonts
-    :commands
-    (cnfonts-enable)
-    :config
-    (setq-default
-     cnfonts-default-step 4
-     cnfonts-directory (concat doom-etc-dir "cnfonts/")
-     cnfonts-personal-fontnames
-     '(
-       ("Noto Mono" "Noto Sans" "Noto Serif")
-       ("Hiragino Sans GB" "Source Han Sans SC" "Source Han Serif SC" "Noto Sans CJK SC" "Noto Sans Mono CJK SC" "Noto Serif CJK SC" "Sarasa Gothic SC" "Sarasa Mono T SC" "Sarasa UI SC" "Sarasa Mono SC")
-       ("Hiragino Sans GB" "Source Han Sans SC" "Source Han Serif SC" "Noto Sans CJK SC" "Noto Sans Mono CJK SC" "Noto Serif CJK SC" "Sarasa Gothic SC" "Sarasa Mono T SC" "Sarasa UI SC" "Sarasa Mono SC")
-       ))
-    (map!
-     :leader
-     :prefix ("t" . "toggle")
-     :desc "Increase Fontsize"  :n "+"  #'cnfonts-increase-fontsize
-     :desc "Decrease Fontsize"  :n "-"  #'cnfonts-decrease-fontsize
-     :desc "Reset Fontsize"     :n "0"  #'cnfonts-reset-fontsize)))
+(def-package! cnfonts
+  :when (display-graphic-p)
+  :init (cnfonts-enable)
+  :config
+  (setq-default
+   cnfonts-default-step 4
+   cnfonts-directory (concat doom-etc-dir "cnfonts/")
+   cnfonts-personal-fontnames
+   '(
+     ("Noto Mono" "Noto Sans" "Noto Serif")
+     ("Hiragino Sans GB" "Source Han Sans SC" "Source Han Serif SC" "Noto Sans CJK SC" "Noto Sans Mono CJK SC" "Noto Serif CJK SC" "Sarasa Gothic SC" "Sarasa Mono T SC" "Sarasa UI SC" "Sarasa Mono SC")
+     ("Hiragino Sans GB" "Source Han Sans SC" "Source Han Serif SC" "Noto Sans CJK SC" "Noto Sans Mono CJK SC" "Noto Serif CJK SC" "Sarasa Gothic SC" "Sarasa Mono T SC" "Sarasa UI SC" "Sarasa Mono SC")
+     ))
+  (map!
+   :leader
+   :prefix ("t" . "toggle")
+   :desc "Increase Fontsize"  :n "+"  #'cnfonts-increase-fontsize
+   :desc "Decrease Fontsize"  :n "-"  #'cnfonts-decrease-fontsize
+   :desc "Reset Fontsize"     :n "0"  #'cnfonts-reset-fontsize))
 
-(when (display-graphic-p)
-  (def-package! fcitx
-    :config
-    (setq-default
-     fcitx-active-evil-states '(insert emacs hybrid))
-    :commands
-    (fcitx-aggressive-setup)))
+(def-package! fcitx
+  :when (display-graphic-p)
+  :config
+  (setq-default
+   fcitx-active-evil-states '(insert emacs hybrid))
+  (fcitx-aggressive-setup))
 
 (def-package! youdao-dictionary
   :config
