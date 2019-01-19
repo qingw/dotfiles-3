dc0ee85dc93a55abaa6283d0b2a2a0e46f66cb9d
SteamedFish <i@steamedfish.org>
2019-01-19 18:02:42 +0800

    Move vterm module compile to autoload

--- .doom.d/modules/emacs/vterm/config.el
+++ .doom.d/modules/emacs/vterm/config.el
@@ -9,20 +9,7 @@
        (concat
         (file-name-directory (locate-library "vterm"))
         "vterm-module.so"))
-    (let ((default-directory (file-name-directory (locate-library "vterm"))))
-      (let* ((buffer (get-buffer-create "Install Vterm"))
-             (status
-              (call-process
-               "sh" nil buffer t "-c"
-               (concat
-                "mkdir -p build;"
-                "cd build;"
-                "cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..;"
-                "make"))))
-        (if (eq status 0)
-            (message "Compilation of emacs-libvterm module succeeded")
-          (pop-to-buffer "Install Vterm")
-          (error "Compilation of emacs-libvterm module failed!")))))
+   (+vterm/vterm-module-compile))
   :config
   (set-env! "SHELL")
   (setq-default
