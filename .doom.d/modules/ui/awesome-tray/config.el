438fa1eb6010aa1e0562c053c2965e71a8146c6f
SteamedFish <i@steamedfish.org>
2019-01-19 22:26:58 +0800

    add awesome-tray, currently not working

---
+++ .doom.d/modules/ui/awesome-tray/config.el
@@ -0,0 +1,4 @@
+;;; ui/awesome-tray/config.el -*- lexical-binding: t; -*-
+
+(after! awesome-tray
+  (awesome-tray-mode 1))
---
+++ .doom.d/modules/ui/awesome-tray/packages.el
@@ -0,0 +1,5 @@
+;; -*- no-byte-compile: t; -*-
+;;; ui/awesome-tray/packages.el
+
+(package! awesome-tray
+  :recipe (:fetcher github :repo "manateelazycat/awesome-tray"))
