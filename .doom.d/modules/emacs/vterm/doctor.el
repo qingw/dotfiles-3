f1a657d9ebf730f40792d3fb290420e5b5906139
SteamedFish <i@steamedfish.org>
2019-01-19 17:49:03 +0800

    add check for make and cmake commands

--- .doom.d/modules/emacs/vterm/doctor.el
+++ .doom.d/modules/emacs/vterm/doctor.el
@@ -2,3 +2,9 @@
 
 (unless (executable-find "vterm-ctrl")
   (warn! "Couldn't find libvterm library. Please install it on your system"))
+
+(unless (executable-find "make")
+  (warn! "Couldn't find make command. Please install it on your system"))
+
+(unless (executable-find "cmake")
+  (warn! "Couldn't find cmake command. Please install it on your system"))
