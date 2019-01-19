31d2d2b32f9599314141592455f31199a76bb3ac
SteamedFish <i@steamedfish.org>
2019-01-19 17:33:12 +0800

    automatically compile vtem if not already

--- .doom.d/modules/emacs/vterm/config.el
+++ .doom.d/modules/emacs/vterm/config.el
@@ -3,6 +3,26 @@
 (add-to-list 'load-path (concat doom-packages-dir "/quelpa/build/vterm"))
 
 (def-package! vterm
+  :init
+  (unless
+      (file-executable-p
+       (concat
+        (file-name-directory (locate-library "vterm"))
+        "vterm-module.so"))
+    (let ((default-directory (file-name-directory (locate-library "vterm"))))
+      (let* ((buffer (get-buffer-create "Install Vterm"))
+             (status
+              (call-process
+               "sh" nil buffer t "-c"
+               (concat
+                "mkdir -p build;"
+                "cd build;"
+                "cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..;"
+                "make"))))
+        (if (eq status 0)
+            (message "Compilation of emacs-libvterm module succeeded")
+          (pop-to-buffer "Install Vterm")
+          (error "Compilation of emacs-libvterm module failed!")))))
   :config
   (set-env! "SHELL")
   (setq-default
