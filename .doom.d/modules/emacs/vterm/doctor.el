dbdd4f5810bc353ec3ee7faa44cae8cb859f6035
SteamedFish <i@steamedfish.org>
2019-01-19 17:37:35 +0800

    we automatically compile the module

--- .doom.d/modules/emacs/vterm/doctor.el
+++ .doom.d/modules/emacs/vterm/doctor.el
@@ -2,8 +2,3 @@
 
 (unless (executable-find "vterm-ctrl")
   (warn! "Couldn't find libvterm library. Please install it on your system"))
-
-(unless
-    (file-executable-p
-     (concat doom-packages-dir "/quelpa/build/vterm/vterm-module.so"))
-  (warn! "Couldn't find vterm-module.so, you have to compile vterm-module"))
