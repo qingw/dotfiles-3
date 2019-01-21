03596d319a025b361dde9085072fb55ad7a78dde
SteamedFish <i@steamedfish.org>
2019-01-22 00:12:44 +0800

    use vterm's own way to compile

--- .doom.d/modules/emacs/vterm/config.el
+++ .doom.d/modules/emacs/vterm/config.el
@@ -8,20 +8,7 @@
        (concat
         (file-name-directory (locate-library "vterm"))
         "vterm-module.so"))
-    (let ((default-directory (file-name-directory (locate-library "vterm"))))
-      (let* ((buffer (get-buffer-create "Install Vterm"))
-             (status
-              (call-process
-               "sh" nil buffer t "-c"
-               (concat
-                "mkdir -p build;"
-                "cd build;"
-                "cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..;"
-                "make"))))
-        (if (eq status 0)
-            (message "Compilation of emacs-libvterm module succeeded")
-          (pop-to-buffer "Install Vterm")
-          (error "Compilation of emacs-libvterm module failed!")))))
+    (setq-default vterm-install t))
   :when (string-match-p "MODULES" system-configuration-features)
   :config
   (set-env! "SHELL")
