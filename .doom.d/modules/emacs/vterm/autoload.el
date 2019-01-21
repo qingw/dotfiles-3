1eb09f5a24b9c9b0e7052edc1cbab145806e109c
SteamedFish <i@steamedfish.org>
2019-01-21 21:44:26 +0800

    Move non autoload function out of autoload.el

--- .doom.d/modules/emacs/vterm/autoload.el
+++ .doom.d/modules/emacs/vterm/autoload.el
@@ -27,26 +27,3 @@ non-nil, cd into the current project's root."
   "Open a terminal popup window in the root of the current project."
   (interactive)
   (+vterm/open-popup t))
-
-;;;###autoload
-(defun +vterm/vterm-module-compile ()
-  "Compile vterm-module.so"
-  (unless
-      (file-executable-p
-       (concat
-        (file-name-directory (locate-library "vterm"))
-        "vterm-module.so"))
-    (let ((default-directory (file-name-directory (locate-library "vterm"))))
-      (let* ((buffer (get-buffer-create "Install Vterm"))
-             (status
-              (call-process
-               "sh" nil buffer t "-c"
-               (concat
-                "mkdir -p build;"
-                "cd build;"
-                "cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..;"
-                "make"))))
-        (if (eq status 0)
-            (message "Compilation of emacs-libvterm module succeeded")
-          (pop-to-buffer "Install Vterm")
-          (error "Compilation of emacs-libvterm module failed!"))))))
--- .doom.d/modules/emacs/vterm/config.el
+++ .doom.d/modules/emacs/vterm/config.el
@@ -8,7 +8,20 @@
        (concat
         (file-name-directory (locate-library "vterm"))
         "vterm-module.so"))
-   (+vterm/vterm-module-compile))
+    (let ((default-directory (file-name-directory (locate-library "vterm"))))
+      (let* ((buffer (get-buffer-create "Install Vterm"))
+             (status
+              (call-process
+               "sh" nil buffer t "-c"
+               (concat
+                "mkdir -p build;"
+                "cd build;"
+                "cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..;"
+                "make"))))
+        (if (eq status 0)
+            (message "Compilation of emacs-libvterm module succeeded")
+          (pop-to-buffer "Install Vterm")
+          (error "Compilation of emacs-libvterm module failed!")))))
   :when (string-match-p "MODULES" system-configuration-features)
   :config
   (set-env! "SHELL")
@@ -21,3 +34,4 @@
      :desc "Terminal"          "t" #'+vterm/open
      :desc "Terminal in popup" "T" #'+vterm/open-popup-in-project))
   (add-hook 'vterm-mode-hook #'doom|mark-buffer-as-real))
+
