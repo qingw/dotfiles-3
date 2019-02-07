43c56634d97db204c080e2aa9f0837b86c0316d8
SteamedFish <i@steamedfish.org>
2019-02-07 22:43:27 +0800

    add more keybindings for orgmode

--- .doom.d/modules/private/keybindings/+org.el
+++ .doom.d/modules/private/keybindings/+org.el
@@ -7,9 +7,11 @@
  :desc "Attach"            "A"  #'org-attach
  :desc "Capture"           "c"  #'org-capture
  :desc "Deadline"          "d"  #'org-deadline
+ :desc "Goto"              "g"  #'org-goto
  :desc "Filter"            "f"  #'org-match-sparse-tree
  :desc "Priority"          "p"  #'org-priority
  :desc "Refile"            "r"  #'org-refile
+ :desc "Reveal"            "R"  #'org-reveal
  :desc "Schedule"          "s"  #'org-schedule
  :desc "Create/Edit Todo"  "t"  #'org-todo
  :desc "Tag heading"       "T"  #'org-set-tags-command)
