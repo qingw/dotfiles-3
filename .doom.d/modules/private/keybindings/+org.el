c086fc8e1f7cfd24dff3318fc5c1fd957ef6ae3b
SteamedFish <i@steamedfish.org>
2019-02-14 11:30:08 +0800

    use tab to show subtree

--- .doom.d/modules/private/keybindings/+org.el
+++ .doom.d/modules/private/keybindings/+org.el
@@ -3,15 +3,16 @@
 (map!
  :map evil-org-mode-map
  :localleader
- :desc "Agenda"            "a"  #'org-agenda
- :desc "Attach"            "A"  #'org-attach
- :desc "Capture"           "c"  #'org-capture
- :desc "Deadline"          "d"  #'org-deadline
- :desc "Goto"              "g"  #'org-goto
- :desc "Filter"            "f"  #'org-match-sparse-tree
- :desc "Priority"          "p"  #'org-priority
- :desc "Refile"            "r"  #'org-refile
- :desc "Reveal"            "R"  #'org-reveal
- :desc "Schedule"          "s"  #'org-schedule
- :desc "Create/Edit Todo"  "t"  #'org-todo
- :desc "Tag heading"       "T"  #'org-set-tags-command)
+ :desc "Show Subtree"      "<tab>" #'org-show-subtree
+ :desc "Agenda"            "a"     #'org-agenda
+ :desc "Attach"            "A"     #'org-attach
+ :desc "Capture"           "c"     #'org-capture
+ :desc "Deadline"          "d"     #'org-deadline
+ :desc "Goto"              "g"     #'org-goto
+ :desc "Filter"            "f"     #'org-match-sparse-tree
+ :desc "Priority"          "p"     #'org-priority
+ :desc "Refile"            "r"     #'org-refile
+ :desc "Reveal"            "R"     #'org-reveal
+ :desc "Schedule"          "s"     #'org-schedule
+ :desc "Create/Edit Todo"  "t"     #'org-todo
+ :desc "Tag heading"       "T"     #'org-set-tags-command)
