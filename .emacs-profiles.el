a833867049b2c08e739226959cab612b8309fdca
SteamedFish <i@steamedfish.org>
2019-02-20 11:28:17 +0800

    Add .emacs-profiles.el for chemacs

---
+++ .emacs-profiles.el
@@ -0,0 +1,8 @@
+;;; .emacs-profiles.el  -*- lexical-binding: t; -*-
+
+
+(("default" . ((user-emacs-directory . "~/.emacs.d")))
+ ("spacemacs" . ((user-emacs-directory . "~/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
+ ("doom" . ((user-emacs-directory . "~/doom-emacs")
+            (env . (("DOOMDIR" . "~/.doom.d"))))))
