5fcaac42c2977600054653a85201054ee5d80ee4
SteamedFish <i@steamedfish.org>
2019-02-23 21:36:21 +0800

    reformat

--- .bashrc.d/linux.sh
+++ .bashrc.d/linux.sh
@@ -4,12 +4,12 @@
 if [ "$OSTYPE" == "linux-gnu" ]; then
     # Change the window title of X terminals
     case ${TERM} in
-      xterm*|rxvt*|Eterm*|aterm|kterm|gnome*|interix|konsole*)
-        PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/\~}\007"'
-        ;;
-      screen*)
-        PROMPT_COMMAND='echo -ne "\033_${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/\~}\033\\"'
-        ;;
+        xterm*|rxvt*|Eterm*|aterm|kterm|gnome*|interix|konsole*)
+            PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/\~}\007"'
+            ;;
+        screen*)
+            PROMPT_COMMAND='echo -ne "\033_${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/\~}\033\\"'
+            ;;
     esac
     if $CLICOLOR ; then
         alias ls='ls --color=auto'
