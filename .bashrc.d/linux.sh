dd9f45172f874f7693fd3d0c050560feae2a1bfe
SteamedFish <i@steamedfish.org>
2019-02-23 21:23:34 +0800

    Linux's path is not the same as mac

--- .bashrc.d/linux.sh
+++ .bashrc.d/linux.sh
@@ -18,7 +18,7 @@ if [ "$OSTYPE" == "linux-gnu" ]; then
     # bash-powerline
     _SITE_PACKAGES=$(python -c "import sys; print(next(p for p in sys.path if 'site-packages' in p))")
     if [ -f $_SITE_PACKAGES/powerline/bindings/bash/powerline.sh ]; then
-        /usr/local/bin/powerline-daemon -q
+        powerline-daemon -q
         POWERLINE_BASH_CONTINUATION=1
         POWERLINE_BASH_SELECT=1
         . $_SITE_PACKAGES/powerline/bindings/bash/powerline.sh
