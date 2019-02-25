37becdfc038563336c81b4870b1d5f33a321b1eb
SteamedFish <i@steamedfish.org>
2019-02-26 00:40:05 +0800

    fix Linux ConTeXt PATH bug

--- .bashrc.d/context.sh
+++ .bashrc.d/context.sh
@@ -3,16 +3,18 @@
 # ConTeXt minimals
 _POSSIBLE_PATH="/Volumes/DATA/ConTeXt /Volumes/MacData/ConTeXt /Volumes/SteamedFish/ConTeXt $HOME/ConTeXt"
 for _CONTEXT_PATH in ${_POSSIBLE_PATH}; do
-    if [ -d "${_CONTEXT_PATH}" ] && [ "$OSTYPE" == "darwin"* ]; then
-        #source ${_CONTEXT_PATH}/tex/setuptex
-        if ! [ -f '/etc/paths.d/ConTeXt' ]; then
-            echo "please run sudo echo ${_CONTEXT_PATH}/tex/texmf-osx-64/bin > /etc/paths.d/ConTeXt"
+    if [ -d "${_CONTEXT_PATH}" ]; then
+        if [[ "$OS_TYPE" == "darwin"* ]]; then
+            if ! [ -f '/etc/paths.d/ConTeXt' ]; then
+                echo "please run sudo echo ${_CONTEXT_PATH}/tex/texmf-osx-64/bin > /etc/paths.d/ConTeXt"
+            fi
+            export OSFONTDIR="/Library/Fonts/;/System/Library/Fonts/;$HOME/Library/Fonts/"
+            break
+        elif [ "$OSTYPE" == "linux-gnu" ]; then
+            export PATH="$PATH:${_POSSIBLE_PATH}/tex/texmf-linux-64/bin"
+            export OSFONTDIR="/usr/local/share/fonts;$HOME/.fonts"
+                break
         fi
-        export OSFONTDIR="/Library/Fonts/;/System/Library/Fonts/;$HOME/Library/Fonts/"
-        break
-    elif [ "$OSTYPE" == "linux-gnu" ]; then
-        export PATH=$PATH:${_POSSIBLE_PATH}/tex/texmf-linux-64/bin
-        export OSFONTDIR="/usr/local/share/fonts;$HOME/.fonts"
     fi
 done
 unset _POSSIBLE_PATH
