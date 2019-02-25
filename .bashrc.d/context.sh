048c7c803917f4a084d6d8fe56a52d2f16e92dc9
SteamedFish <i@steamedfish.org>
2019-02-26 00:34:32 +0800

    Linux support for ConTeXt

--- .bashrc.d/context.sh
+++ .bashrc.d/context.sh
@@ -3,13 +3,16 @@
 # ConTeXt minimals
 _POSSIBLE_PATH="/Volumes/DATA/ConTeXt /Volumes/MacData/ConTeXt /Volumes/SteamedFish/ConTeXt $HOME/ConTeXt"
 for _CONTEXT_PATH in ${_POSSIBLE_PATH}; do
-    if [ -d "${_CONTEXT_PATH}" ]; then
+    if [ -d "${_CONTEXT_PATH}" ] && [ "$OSTYPE" == "darwin"* ]; then
         #source ${_CONTEXT_PATH}/tex/setuptex
         if ! [ -f '/etc/paths.d/ConTeXt' ]; then
             echo "please run sudo echo ${_CONTEXT_PATH}/tex/texmf-osx-64/bin > /etc/paths.d/ConTeXt"
         fi
-        OSFONTDIR="/Library/Fonts/;/System/Library/Fonts/;$HOME/Library/Fonts/"
+        export OSFONTDIR="/Library/Fonts/;/System/Library/Fonts/;$HOME/Library/Fonts/"
         break
+    elif [ "$OSTYPE" == "linux-gnu" ]; then
+        export PATH=$PATH:${_POSSIBLE_PATH}/tex/texmf-linux-64/bin
+        export OSFONTDIR="/usr/local/share/fonts;$HOME/.fonts"
     fi
 done
 unset _POSSIBLE_PATH
