b19a95c7d99a6d72d6511df7621de12c2c38fd30
SteamedFish <i@steamedfish.org>
2019-02-28 01:12:23 +0800

    autoload emacs

--- .bashrc.d/emacs.sh
+++ .bashrc.d/emacs.sh
@@ -7,7 +7,34 @@ if [ -n "$INSIDE_EMACS" ]; then
     alias emacs="emacsclient -n"
 fi
 
-alias e="emacsclient -n"
+function emacs {
+    if [[ $# -eq 0 ]]; then
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            /Applications/Emacs.app/Contents/MacOS/Emacs
+        elif [ "$OSTYPE" == "linux-gnu" ]; then
+            /usr/bin/emacs # "emacs" is function, will cause recursion
+        fi
+        return
+    fi
+    args=($*)
+    for ((i=0; i <= ${#args}; i++)); do
+        local a=${args[i]}
+        # NOTE: -c for creating new frame
+        if [[ ${a:0:1} == '-' && ${a} != '-c' && ${a} != '--' ]]; then
+            if [[ "$OSTYPE" == "darwin"* ]]; then
+                /Applications/Emacs.app/Contents/MacOS/Emacs ${args[*]}
+            elif [ "$OSTYPE" == "linux-gnu" ]; then
+                /usr/bin/emacs ${args[*]}
+            fi
+            return
+        fi
+    done
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        setsid emacsclient -n -a /Applications/Emacs.app/Contents/MacOS/Emacs ${args[*]}
+    elif [ "$OSTYPE" == "linux-gnu" ]; then
+        setsid emacsclient -n -a /usr/bin/emacs ${args[*]}
+    fi
+}
 
 if [ -d "$HOME/emacs-distros/spacemacs" ] && \
    [ -f "$HOME/.emacs-profiles.el" ] && \
