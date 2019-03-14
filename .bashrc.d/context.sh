ce31a044bdddaab0cfbae699ce17e5fc9c17b81a
SteamedFish <i@steamedfish.org>
2019-03-14 11:22:33 +0800

    no break because we don't have loops now

--- .bashrc.d/context.sh
+++ .bashrc.d/context.sh
@@ -7,10 +7,8 @@ if [ -d "$HOME/ConTeXt" ]; then
             echo "please run sudo echo $HOME/ConTeXt/tex/texmf-osx-64/bin > /etc/paths.d/ConTeXt"
         fi
         export OSFONTDIR="/Library/Fonts/;/System/Library/Fonts/;$HOME/Library/Fonts/"
-        break
     elif [ "$OSTYPE" == "linux-gnu" ]; then
         export PATH="$PATH:$HOME/ConTeXt/tex/texmf-linux-64/bin"
         export OSFONTDIR="/usr/local/share/fonts;$HOME/.fonts"
-        break
     fi
 fi
