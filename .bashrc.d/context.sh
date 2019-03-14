49cb85c84bf3298cfa1a1020d2dab6209b4963e6
SteamedFish <i@steamedfish.org>
2019-03-14 11:20:48 +0800

    Move all computer's ConTeXt into the same location

--- .bashrc.d/context.sh
+++ .bashrc.d/context.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
 # ConTeXt minimals
-_POSSIBLE_PATH="/Volumes/DATA/ConTeXt /Volumes/MacData/ConTeXt /Volumes/SteamedFish/ConTeXt $HOME/ConTeXt"
-for _CONTEXT_PATH in ${_POSSIBLE_PATH}; do
-    if [ -d "${_CONTEXT_PATH}" ]; then
-        if [[ "$OS_TYPE" == "darwin"* ]]; then
-            if ! [ -f '/etc/paths.d/ConTeXt' ]; then
-                echo "please run sudo echo ${_CONTEXT_PATH}/tex/texmf-osx-64/bin > /etc/paths.d/ConTeXt"
-            fi
-            export OSFONTDIR="/Library/Fonts/;/System/Library/Fonts/;$HOME/Library/Fonts/"
-            break
-        elif [ "$OSTYPE" == "linux-gnu" ]; then
-            export PATH="$PATH:${_CONTEXT_PATH}/tex/texmf-linux-64/bin"
-            export OSFONTDIR="/usr/local/share/fonts;$HOME/.fonts"
-            break
+if [ -d "$HOME/ConTeXt" ]; then
+    if [[ "$OS_TYPE" == "darwin"* ]]; then
+        if ! [ -f '/etc/paths.d/ConTeXt' ]; then
+            echo "please run sudo echo $HOME/ConTeXt/tex/texmf-osx-64/bin > /etc/paths.d/ConTeXt"
         fi
+        export OSFONTDIR="/Library/Fonts/;/System/Library/Fonts/;$HOME/Library/Fonts/"
+        break
+    elif [ "$OSTYPE" == "linux-gnu" ]; then
+        export PATH="$PATH:$HOME/ConTeXt/tex/texmf-linux-64/bin"
+        export OSFONTDIR="/usr/local/share/fonts;$HOME/.fonts"
+        break
     fi
-done
-unset _POSSIBLE_PATH
-unset _CONTEXT_PATH
+fi
