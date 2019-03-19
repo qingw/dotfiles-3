c1f850bef88ec181e0a6f1b06ad297f875695dde
SteamedFish <i@steamedfish.org>
2019-03-19 21:22:02 +0800

    Enable rest client

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -140,7 +140,7 @@
         +ipython)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
-       ;;rest              ; Emacs as a REST client
+       rest              ; Emacs as a REST client
        ;;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
