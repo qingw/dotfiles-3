5bc4b7daa209abf27c486b30010d74315187764d
SteamedFish <i@steamedfish.org>
2019-05-04 16:06:19 +0800

    setup org-publish

--- .doom.d/modules/private/steamedfish/config.el
+++ .doom.d/modules/private/steamedfish/config.el
@@ -63,7 +63,34 @@
                 org-archive-location
                 (concat org-directory "archive.org::* From %s")
                 org-log-done 'time
-                org-log-done-with-time t))
+                org-log-done-with-time t)
+
+  ;; setup blog publish
+  (unless (boundp 'org-publish-project-alist)
+    (setq org-publish-project-alist nil))
+  (add-to-list 'org-publish-project-alist
+               `("org-blog"
+                 ;; Path to your org files.
+                 :base-directory "~/work/SteamedFish.github.io/org/"
+                 :base-extension "org"
+
+                 ;; Path to your Jekyll project.
+                 :publishing-directory "~/work/SteamedFish.github.io/"
+                 :recursive t
+                 :publishing-function org-html-publish-to-html
+                 :language cn
+                 :headline-levels 4
+                 :html-extension "html"
+                 :body-only t)) ;; Only export section between <body> </body>
+  (add-to-list 'org-publish-project-alist
+               `("org-blog-static"
+                 :base-directory "~/work/SteamedFish.github.io/org/"
+                 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
+                 :publishing-directory "~/work/SteamedFish.github.io/"
+                 :recursive t
+                 :publishing-function org-publish-attachment))
+  (add-to-list 'org-publish-project-alist
+               `("blog" :components ("org-blog" "org-blog-static"))))
 
 (after! projectile
   (projectile-add-known-project "~/dotfiles")
