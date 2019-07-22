bdefae6f96a86ba923a0235331831606cf8bbf9a
SteamedFish <i@steamedfish.org>
2019-07-22 22:27:12 +0800

    remove fcitx

--- .doom.d/modules/intl/chinese/+fcitx.el
+++
@@ -1,9 +0,0 @@
-;;; intl/chinese/+fcitx.el -*- lexical-binding: t; -*-
-
-(def-package! fcitx
-  :config
-  (if IS-LINUX
-      (setq fcitx-use-dbus t)))
-    ;; Mac's fcitx causes hangup
-    ;; (setq fcitx-active-evil-states '(insert emacs hybrid)
-    ;; (fcitx-aggressive-setup)
--- .doom.d/modules/intl/chinese/packages.el
+++ .doom.d/modules/intl/chinese/packages.el
@@ -1,8 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/dotfiles/.doom.d/modules/intl/chinese/packages.el
 
-(if (featurep! fcitx)
-    (package! fcitx))
 (if (featurep! cnfontx)
     (package! cnfonts))
 (package! youdao-dictionary)
