390fe6b706d0c84b46e4170239e163d22afe0643
SteamedFish <i@steamedfish.org>
2019-08-20 15:51:55 +0800

    remove org publish stuff

--- .doom.d/modules/private/steamedfish/config.el
+++ .doom.d/modules/private/steamedfish/config.el
@@ -78,34 +78,7 @@
         org-ellipsis (if (char-displayable-p ?⬎) "  ⬎" nil))
 
   ;; refresh images after C-c C-c
-  (add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images)
-
-  ;; setup blog publish
-  (unless (boundp 'org-publish-project-alist)
-    (setq org-publish-project-alist nil))
-  (add-to-list 'org-publish-project-alist
-               `("org-blog"
-                 ;; Path to your org files.
-                 :base-directory "~/work/SteamedFish.github.io/org/"
-                 :base-extension "org"
-
-                 ;; Path to your Jekyll project.
-                 :publishing-directory "~/work/SteamedFish.github.io/_posts"
-                 :recursive t
-                 :publishing-function org-html-publish-to-html
-                 :language cn
-                 :headline-levels 4
-                 :html-extension "html"
-                 :body-only t)) ;; Only export section between <body> </body>
-  (add-to-list 'org-publish-project-alist
-               `("org-blog-static"
-                 :base-directory "~/work/SteamedFish.github.io/org/"
-                 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
-                 :publishing-directory "~/work/SteamedFish.github.io/_posts"
-                 :recursive t
-                 :publishing-function org-publish-attachment))
-  (add-to-list 'org-publish-project-alist
-               `("blog" :components ("org-blog" "org-blog-static"))))
+  (add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images))
 
 (after! projectile
   (projectile-add-known-project "~/dotfiles")
