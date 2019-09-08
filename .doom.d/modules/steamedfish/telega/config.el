5e5b8bc56603765db5d2cfd95558588bfd26891c
SteamedFish <i@steamedfish.org>
2019-09-08 16:02:49 +0800

    set telega popup rule

--- .doom.d/modules/steamedfish/telega/config.el
+++ .doom.d/modules/steamedfish/telega/config.el
@@ -1,13 +1,11 @@
 ;;; steamedfish/telega/config.el -*- lexical-binding: t; -*-
 
-
 (use-package! telega
   :commands (telega)
   :defer t
-  :config)
-  ;; (setq telega-debug t))
-  ;; (setq telega-use-notifications t)
-  ;; (add-hook telega-chat-mode-hook
-  ;;           (lambda ()
-  ;;             (setq company-backends '(telega-company-emoji))
-  ;;             (company-mode 1))))
+  :config
+  (telega-mode-line-mode 1)
+  (set-popup-rule!
+    ("^\\*Telega Root" :side 'right :size 0.2 :quit nil))
+  (set-popup-rule!
+    ("^◀\\[.*@.*\\]" :side 'left :size 0.6 :quit nil)))
