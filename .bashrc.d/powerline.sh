2e88fb71a1ef32ff8531f64ef1fed1a58bf2b851
SteamedFish <i@steamedfish.org>
2019-09-09 16:18:56 +0800

    Revert "update debian settings"
    This reverts commit 65626d7f1f5b535561a65aa9b7688261929dbed8.

--- .bashrc.d/powerline.sh
+++ .bashrc.d/powerline.sh
@@ -16,14 +16,15 @@ if [ "$TERM_PROGRAM" != "iTerm.app" ] && [ "$OSTYPE" != "linux-gnu" ]; then
 fi
 
 if [ -f /etc/debian_version ]; then
-    source /usr/share/powerline/integrations/powerline.sh
+    _SITE_PACKAGES="/usr/share/"
 else
     _SITE_PACKAGES=$(python -c "import sys; print(next(p for p in sys.path if 'site-packages' in p and '.local' not in p))")
-    if [ -f $_SITE_PACKAGES/powerline/bindings/bash/powerline.sh ]; then
+fi
+
+if [ -f $_SITE_PACKAGES/powerline/bindings/bash/powerline.sh ]; then
         powerline-daemon -q
         POWERLINE_BASH_CONTINUATION=1
         POWERLINE_BASH_SELECT=1
         . $_SITE_PACKAGES/powerline/bindings/bash/powerline.sh
-    fi
 fi
 unset _SITE_PACKAGES
