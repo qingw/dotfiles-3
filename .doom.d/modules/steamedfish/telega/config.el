871b72ac6f13cad7e211eb55f2677dd64c7db83d
SteamedFish <i@steamedfish.org>
2019-09-09 10:28:29 +0800

    currect telega popup settings

--- .doom.d/modules/steamedfish/telega/config.el
+++ .doom.d/modules/steamedfish/telega/config.el
@@ -5,7 +5,5 @@
   :defer t
   :config
   (telega-mode-line-mode 1)
-  (set-popup-rule!
-    ("^\\*Telega Root" :side 'right :size 0.2 :quit nil))
-  (set-popup-rule!
-    ("^◀\\[.*@.*\\]" :side 'left :size 0.6 :quit nil)))
+  (set-popup-rule! "^\\*Telega Root" :side 'left :size 0.2 :quit nil)
+  (set-popup-rule! "^◀\\[.*@.*\\]" :side 'right :size 0.6 :quit nil))
