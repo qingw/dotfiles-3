0f7f54d1d44af43d096e5278ea95c79a3c3ef54e
SteamedFish <i@steamedfish.org>
2019-09-13 22:53:25 +0800

    ivy support of telega

--- .doom.d/modules/steamedfish/telega/config.el
+++ .doom.d/modules/steamedfish/telega/config.el
@@ -1,9 +1,18 @@
 ;;; steamedfish/telega/config.el -*- lexical-binding: t; -*-
 
+;; copied from https://github.com/sarg/dotfiles/
 (use-package! telega
   :commands (telega)
   :defer t
+  :hook (telega-chat-mode . doom-mark-buffer-as-real-h)
   :config
   (telega-mode-line-mode 1)
-  (set-popup-rule! "^\\*Telega Root" :side 'left :size 0.2 :quit nil)
-  (set-popup-rule! "^◀\\[.*@.*\\]" :side 'right :size 0.6 :quit nil))
+  (set-popup-rule! "^\\*Telega Root" :side 'left :size 0.2 :quit nil :select t)
+  (set-popup-rule! "^◀\\[.*@.*\\]" :side 'right :size 0.6 :quit nil :select t)
+  (when (featurep! :completion ivy)
+    (load! "+ivy"))
+  (when (featurep! :editor evil)
+    (map!
+     (:map telega-msg-button-map
+       "k" nil
+       "l" nil))))
