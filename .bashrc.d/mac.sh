457a7978c2d7488160c3cafd6a280cd551e20b2b
SteamedFish <i@steamedfish.org>
2019-09-16 22:23:28 +0800

    add homebrew bottle mirror

--- .bashrc.d/mac.sh
+++ .bashrc.d/mac.sh
@@ -4,6 +4,8 @@ if [[ "$OSTYPE" != "darwin"* ]]; then
     return
 fi
 
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles
+
 # envs from pass
 if [ -n "$(command -v pass)" ]; then
     export HOMEBREW_GITHUB_API_TOKEN="$(pass env/homebrew)"
