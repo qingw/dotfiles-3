581a2886a11a734c8dddb5b3d8dd28da7958477a
SteamedFish <i@steamedfish.org>
2019-09-16 14:19:28 +0800

    automatically setup proxy on macOS

--- bin/netease-homeproxy.sh
+++ bin/netease-homeproxy.sh
@@ -1,1 +1,9 @@
 ssh -D 2080 -qfCNnT 192.168.1.30
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    for networkservice in Ethernet Wi-Fi; do
+        # This must be a valid URL. macOS doesn't support file based pac address.
+        networksetup -setautoproxyurl $networkservice https://steamedfish.org/netease-homeproxy.pac > /dev/null 2>&1 || true
+        networksetup -setautoproxystate $networkservice on > /dev/null 2>&1 || true
+    done
+fi
