1c87ea76c7af69dfd1241518215690594fc6a63f
SteamedFish <i@steamedfish.org>
2019-09-29 15:35:35 +0800

    Add fork to disable svg-embed on emacs-mac

--- .doom.d/modules/steamedfish/telega/packages.el
+++ .doom.d/modules/steamedfish/telega/packages.el
@@ -5,4 +5,5 @@
   (:host github
    :repo "zevlg/telega.el"
    :branch "master"
+   :fork (:host nil :repo "git@github.com:SteamedFish/telega.el.git")
    :files (:defaults "README.md" "etc" "server" "Makefile" "test.el")))
