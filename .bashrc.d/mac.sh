d8f8f969f56caeaf8c764c367a90bb4d6068ccbe
SteamedFish <i@steamedfish.org>
2019-10-08 09:54:01 +0800

    disable tencent mirror
    it syncs too slow

--- .bashrc.d/mac.sh
+++ .bashrc.d/mac.sh
@@ -4,7 +4,7 @@ if [[ "$OSTYPE" != "darwin"* ]]; then
     return
 fi
 
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles
+# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles
 
 # envs from pass
 if [ -n "$(command -v pass)" ]; then
